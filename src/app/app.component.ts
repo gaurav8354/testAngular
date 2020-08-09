ee594806ee56f24290b815b0586db9a8ad127354
gaurav <gaurav18.2013@gmail.com>
2020-08-09 20:48:11 +0530

    final deployment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,74 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'my-first-app';
+  student = 'Chinu';
+  numA = '';
+  numB = '';
+  numValue = 0;
+  opr = '';
+  mySelect = '2';
+  operation = '';
+
+  data = [
+    { operator: '+' },
+    { operator: '-' },
+    { operator: '*' },
+    { operator: '/' },
+  ];
+  selectChange() {
+    console.log(this.mySelect);
+    this.numValue = 0;
+    this.getOperatorValue(true);
+  }
+  calculate() {
+    this.getOperatorValue(false);
+  }
+
+  getOperatorValue(x) {
+    switch (this.mySelect) {
+      case '+':
+        if (x) {
+          this.operation = 'Sum';
+        } else {
+          this.numValue = parseInt(this.numA) + parseInt(this.numB);
+        }
+        break;
+      case '-':
+        if (x) {
+          this.operation = 'Subtraction';
+        } else {
+          this.numValue = parseInt(this.numA) - parseInt(this.numB);
+        }
+        break;
+      case '*':
+        if (x) {
+          this.operation = 'Multiplication';
+        } else {
+          this.numValue = parseInt(this.numA) * parseInt(this.numB);
+        }
+        break;
+      case '/':
+        if (x) {
+          this.operation = 'Division';
+        } else {
+          this.numValue = parseInt(this.numA) / parseInt(this.numB);
+        }
+        break;
+      default:
+        break;
+    }
+  }
+  // title = 'Gauri App';
+  // name = 'Chinu';
+  // firstName = 'Gauri';
+  // lastName = 'xyz';
+  // myClass = 'b';
+  // myColorA = 'Blue';
+
+  // a = 10;
+  // b = 20;
+  // c = 30;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './home/home.component';
+import { CartComponent } from './cart/cart.component';
+import { FormsModule } from '@angular/forms';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HomeComponent, CartComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
